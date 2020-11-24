cc0cbfe0cf3d464d32b7e4f6430d8fedaaa3c311
zzemlyanaya <zem.jane@gmail.com>
2020-11-24 22:25:56 +0500

    v1: final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+/**
+ * @author Evgeniya Zemlyanaya @zzemlyanaya
+ */
 import logo from './logo.svg';
+import rocket from './rocket.svg';
 import './App.css';
+
+function NameText(props) {
+ return <h1 className="name">{props.name}</h1>;
+}
+
+function TextLink(props) {
+ return <a href={props.link} target="_blank" className="link"> {props.text}</a>;
+}
+
+function ImageLogo(){
+  return <img src={logo} alt="logo" />
+}
+
+function ImageRocket(){
+  return <img className="imgSmall" src={rocket} alt="whooosh!" />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className="block">
+         <p> <NameText name="Евгения Земляная" /> </p>
+         <p> <TextLink link="https://github.com/zzemlyanaya" text="@zzemlyanaya" /> </p>
+        <ImageRocket />
+      </div>
+      <div className="block">
+        <ImageLogo />
+      </div>
     </div>
   );
 }
